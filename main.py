42bb0fc2db0d7680ef85f77979872a3d33e3db8f
骆昊 <jackfrued@126.com>
2025-05-28 09:43:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
+import warnings
+
 import streamlit as st
 from langchain.chains.conversation.base import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain_openai import ChatOpenAI
 
+warnings.filterwarnings('ignore')
 
-def get_ai_response(user_prompt, session_state):
+
+def get_ai_response(user_prompt):
     try:
         model = ChatOpenAI(
             model='gpt-4o-mini',
-            api_key=session_state['api_key'],
+            api_key=st.secrets['API_KEY'],
             base_url='https://twapi.openai-hk.com/v1'
         )
-        chain = ConversationChain(llm=model, memory=session_state['memory'])
+        chain = ConversationChain(llm=model, memory=st.session_state['memory'])
         return chain.invoke({'input': user_prompt})['response']
     except Exception as err:
-        return '无法获取服务器响应……'
+        return '暂时无法获取服务器响应……'
 
 
 st.title('我的ChatGPT')
-
-with st.sidebar:
-    api_key = st.text_input('请输入自己专属的API秘钥：', type='password')
-    st.session_state['api_key'] = api_key
 
 if 'messages' not in st.session_state:
     st.session_state['messages'] = [{'role': 'ai', 'content': '你好主人，我是你的AI助手，我叫小美。'}]
@@ -33,13 +33,10 @@ for message in st.session_state['messages']:
 
 user_input = st.chat_input()
 if user_input:
-    if not api_key:
-        st.info("请输入自己专属的API秘钥！！！")
-        st.stop()
     st.chat_message('human').write(user_input)
     st.session_state['messages'].append({'role': 'human', 'content': user_input})
     with st.spinner('AI正在思考，请等待……'):
-        resp_from_ai = get_ai_response(user_input, st.session_state)
+        resp_from_ai = get_ai_response(user_input)
         st.session_state['history'] = resp_from_ai
         st.chat_message('ai').write(resp_from_ai)
         st.session_state['messages'].append({'role': 'ai', 'content': resp_from_ai})
